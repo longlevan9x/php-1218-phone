e361e6571cb0e46db1b50a4f86792cdd57f08974
LongPC <vanlong121996@gmail.com>
2019-06-19 19:16:30 +0700

    update home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Phone;
 class HomeController extends Controller
 {
     /**
@@ -13,7 +13,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('guest');
     }
 
     /**
@@ -23,6 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $phones = Phone::query()->paginate(2);
+        return view('home.home', ['phones' => $phones]);
     }
 }
